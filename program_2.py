a117a92750aed99f4da8be714f364621d5491892
unknown <cloudiebro@hotmail.com>
2021-08-29 15:12:53 -0700

    finished the program

--- program_2.py
+++ program_2.py
@@ -29,7 +29,8 @@ def contains_uppercase(words: str):
     return False
 
 
-def findss(c: str, b: str):
+# let c be the string of the pets name and b is the string of the password
+def find_pet_name(c: str, b: str):
     
     found =  False
     idx = -1
@@ -67,23 +68,54 @@ def main():
     bday = input("Enter your birthdate ex: 11/11/11 : ")
     fav_num = input("Enter your favorite number :")
     num_wrong = 0
-    
+
+    fails = 0
+    strength = "None"
+
     while start_prog:
         
         pw = input("Enter your password: ")
         
         if len(pw) > 40:
             print("Greater than 40 characters")
+            fails += 1
         elif ord(pw[:1]) < 65 or ord(pw[:1]) > 122:
             print("First character is not a letter")
+            fails += 1
         elif contains_uppercase(pw) == False:
             print("No Uppercase")
+            fails += 1
         elif pw.isalnum() == True:
             print("No special characters")
+            fails += 1
         elif " " in pw:
             print("Has spaces")
+            fails += 1
+        elif find_pet_name(pet_name, pw) == False:
+            print("pet name is not avaliable")
+            fails += 1
+        elif pet_name in pw:
+            strength = "Weaker"
+        elif fav_num in pw:
+            strength = "Weaker"
         elif len(pw) == 1:
             if pw == 'Q':
                 return
+
+        if fails == 5:
+             print("Each of the things should not be greater than 40 characters")
+             print("The password must start with a letter")
+             print("There must be atleast 1 uppercase character")
+             print("There must be at least one symbol")
+             print("There can't be any spaces or tabs")
+             print("Press Q to quit")
+             fails = 0
+        elif fails == 0:
+            if bday[len(bday)-4:] in pw[len(pw)-4:]:
+                strength = "Weaker"
+            elif pw[0] == last_name[0]:
+                strength = "Weaker"
+            print("The Password is ", strength)
+    
 
 main()
